1a5c2dfa1c4f50c45fe1baa8ca2327702227eaf4
surtsey <surtsey@4df3a783-e634-0410-9664-e38d53c70025>
2007-07-16 23:41:47 +0000

    Changed a few copyright notices

--- trunk/system/config/routes.inc.php
+++ trunk/system/config/routes.inc.php
@@ -12,7 +12,7 @@
  * @package Archetype
  * @subpackage config
  * @author Justin Krueger <fuzzywoodlandcreature@gmail.com>
- * @copyright Â© 2007 Justin Krueger.  All rights reserved.
+ * @copyright © 2007 Justin Krueger.  All rights reserved.
  * @license http://www.opensource.org/licenses/mit-license.html MIT
  * @link http://fuzzywoodlandcreature.com/archetype
  * @version 2007.4.1
