3d56f967f243983bca2c8f3718dda347aae5cdaf
surtsey <surtsey@4df3a783-e634-0410-9664-e38d53c70025>
2007-07-18 06:05:08 +0000

    Added blueprint for statistic tracker

---
+++ trunk/system/components/stats/automator.inc.php
@@ -0,0 +1,29 @@
+<?php if(!defined('ARCHETYPE_VERSION')){die();}
+
+   ////////////////////////////////////////////////////////////////////
+   //                P R O J E C T A R C H E T Y P E                 //
+   //                 www.fuzzywoodlandcreature.net                  //
+   ////////////////////////////////////////////////////////////////////
+
+/**
+ * Stats automator
+ *
+ * @package Archetype
+ * @subpackage stats
+ * @author Justin Krueger <fuzzywoodlandcreature@gmail.com>
+ * @copyright © 2007 Justin Krueger.  All rights reserved.
+ * @license http://www.opensource.org/licenses/mit-license.html MIT
+ * @link http://fuzzywoodlandcreature.net/archetype
+ * @version 2007.4.1
+ */
+
+/**
+ * Automatically record statistics on each system execution
+ */
+   class stats_automator extends Archetype_automator
+      {
+         public function destruct()
+            {
+            }
+      }
+?>
