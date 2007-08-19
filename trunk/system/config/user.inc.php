eaa13f452431a1ea1adc24be07b7cbe3923f9338
surtsey <surtsey@4df3a783-e634-0410-9664-e38d53c70025>
2007-08-19 11:10:48 +0000

    better.

---
+++ trunk/system/config/user.inc.php
@@ -0,0 +1,21 @@
+<?php if(!defined('ARCHETYPE_VERSION')){die();}
+
+   ////////////////////////////////////////////////////////////////////
+   //                P R O J E C T A R C H E T Y P E                 //
+   //                 www.fuzzywoodlandcreature.net                  //
+   ////////////////////////////////////////////////////////////////////
+
+/**
+ * User config
+ *
+ * @package Archetype
+ * @subpackage config
+ * @author Justin Krueger <fuzzywoodlandcreature@gmail.com>
+ * @copyright © 2007 Justin Krueger.  All rights reserved.
+ * @license http://www.opensource.org/licenses/mit-license.html MIT
+ * @link http://fuzzywoodlandcreature.com/archetype
+ * @version 2007.8.19
+ */
+
+   $user['hash_salt']='3j.^r9%#'; // Don't change this after any users exist
+?>
