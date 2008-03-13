d49800b6c2d778612066fc2c4900f563017e1c9c
surtsey <surtsey@4df3a783-e634-0410-9664-e38d53c70025>
2008-03-13 09:33:23 +0000

    Began work on Archetype.php, merged contents of global.inc.php into it, began moving everything around on the filesystem.  Also started the work on removing injectors.

--- branches/newfslayout/Archetype/Archetype.php
+++ branches/newfslayout/Archetype/Archetype.php
@@ -1,4 +1,5 @@
 <?php
+// TODO Remove injectors, merge global and Archetype files, make Archetype class and put root program in it TODO
 
    ////////////////////////////////////////////////////////////////////
    //                P R O J E C T A R C H E T Y P E                 //
@@ -14,7 +15,7 @@
  * @copyright © 2007 Justin Krueger.  All rights reserved.
  * @license http://www.opensource.org/licenses/mit-license.html MIT
  * @link http://fuzzywoodlandcreature.com/archetype
- * @version 2007.10.22
+ * @version 2008.3.13
  */
 
 // Audible errors because we can't have them ignored for development purposes (feel free to silence a production application if you're sure it's safe)
@@ -22,37 +23,21 @@
 
 /**
  * Archetype's version
- */define('A_VERSION','2007.10.22');
+ */define('A_VERSION','2008.3.13');
 
-/**
- * How should Archetype run?  0 for http, 1 for interactive
- */
-   if(!defined('A_MODE')) { define('A_MODE',0); }
+   if(!defined('A_ARCHETYPE_LOCATION'))   { define('A_ARCHETYPE_LOCATION',   dirname(__FILE__).'/'); }
 
-/**
- * Location of the system directory
- */if(!defined('A_SYSTEM_LOCATION')) { define('A_SYSTEM_LOCATION','Archetype/'); }
-/**
- * Location of automator storage
- */if(!defined('A_AUTOMATORS_LOCATION')) { define('A_AUTOMATORS_LOCATION',A_SYSTEM_LOCATION.'automators/'); }
-/**
- * Location of injector storage
- */if(!defined('A_INJECTORS_LOCATION')) { define('A_INJECTORS_LOCATION',A_SYSTEM_LOCATION.'injectors/'); }
-/**
- * Location of config storage
- */if(!defined('A_SETTINGS_LOCATION')) { define('A_SETTINGS_LOCATION',A_SYSTEM_LOCATION.'settings/'); }
-/**
- * Location of model storage
- */if(!defined('A_MODELS_LOCATION')) { define('A_MODELS_LOCATION',A_SYSTEM_LOCATION.'models/'); }
-/**
- * Location of view storage
- */if(!defined('A_VIEWS_LOCATION')) { define('A_VIEWS_LOCATION',A_SYSTEM_LOCATION.'views/'); }
-/**
- * Location of controller storage
- */if(!defined('A_CONTROLLERS_LOCATION')) { define('A_CONTROLLERS_LOCATION',A_SYSTEM_LOCATION.'controllers/'); }
-/**
- * Location of global classes
- */if(!defined('A_GLOBAL_LOCATION')) { define('A_GLOBAL_LOCATION',A_SYSTEM_LOCATION.'global.inc.php'); }
+   if(!defined('A_SYSTEM_LOCATION'))      { define('A_SYSTEM_LOCATION',      A_ARCHETYPE_LOCATION.'system/'); }
+   if(!defined('A_APPLICATION_LOCATION')) { define('A_APPLICATION_LOCATION', A_ARCHETYPE_LOCATION.'application/'); }
+
+   if(!defined('A_AUTOMATORS_ID'))  { define('A_AUTOMATORS_ID',  A_ARCHETYPE_ID.''); }
+   if(!defined('A_SETTINGS_ID'))    { define('A_SETTINGS_ID',    A_ARCHETYPE_ID.''); }
+   if(!defined('A_GLOBAL_ID'))      { define('A_GLOBAL_ID',      A_ARCHETYPE_ID.''); }
+   if(!defined('A_MODELS_ID'))      { define('A_MODELS_ID',      A_ARCHETYPE_ID.''); }
+   if(!defined('A_VIEWS_ID'))       { define('A_VIEWS_ID',       A_ARCHETYPE_ID.''); }
+   if(!defined('A_CONTROLLERS_ID')) { define('A_CONTROLLERS_ID', A_ARCHETYPE_ID.''); }
+
+   die(A_GLOBAL_LOCATION);
 
 /**
  * Universal variable passed between every object extended from Archetype
@@ -81,9 +66,6 @@
                'controllers'=>array('system'=>false)
             ),
       );
-
-// Open up our global class definitions
-   require(A_GLOBAL_LOCATION);
 
 // Scan the filesystem for injectors
    $_['information']['lists']['injectors']=array_slice(scandir(A_INJECTORS_LOCATION),2);
@@ -177,6 +159,201 @@
          else
             {
                trigger_error($x->__toString(),E_USER_ERROR); // __toString() because PHP5.1 is stupid and because magic methods are sluggish
+            }
+      }
+
+/**
+ * Archetype's exception class
+ */
+   class A_Exception extends Exception {}
+
+/**
+ * Primary class for Archetype.  It should be extended in some form by every other class in the system.
+ */
+   class A
+      {
+      /**
+       * Assigned a reference to $_ (the universal variable) in the constructor
+       * @access public
+       * @var mixed
+       */
+         public $_=false;
+
+      /**
+       * Assigned a reference to $_['objects']['models']['system'] in the constructor and inherited by every class in the system
+       * @access public
+       * @var mixed
+       */
+         public $system=false;
+
+      /**
+       * Dummy constructor
+       * @access public
+       * @return void
+       */
+         public function construct() {}
+
+      /**
+       * Dummy destructor
+       * @access public
+       * @return void
+       */
+         public function destruct() {}
+
+      /**
+       * Constructor that runs in every descendant
+       * @access public
+       * @param array $_ Reference to Archetype's universal variable
+       * @return void
+       */
+         public function __construct(&$_)
+            {
+            // Set the reference to the universal array
+               $this->_=&$_;
+
+            // Load the system model
+               $this->system=&$this->_['objects']['models']['system'];
+
+               if(!empty($this->_['objects']['injectors']))
+                  {
+                  // Loop pre_construct()
+                     foreach($this->_['objects']['injectors'] as &$injector)
+                        {
+                           $injector->pre_construct($this);
+                        }
+
+                     $this->construct();
+
+                  // Loop post_construct()
+                     foreach($this->_['objects']['injectors'] as &$injector)
+                        {
+                           $injector->post_construct($this);
+                        }
+                  }
+               else
+                  {
+                     $this->construct();
+                  }
+            }
+
+      /**
+       * Destructor that runs in every descendant
+       * @access public
+       * @return void
+       */
+         public function __destruct()
+            {
+               if(!empty($this->_['objects']['injectors']))
+                  {
+                  // Loop pre_destruct()
+                     foreach($this->_['objects']['injectors'] as &$injector)
+                        {
+                           $injector->pre_destruct($this);
+                        }
+
+                     $this->destruct();
+
+                  // Loop post_destruct()
+                     foreach($this->_['objects']['injectors'] as &$injector)
+                        {
+                           $injector->post_destruct($this);
+                        }
+                  }
+               else
+                  {
+                     $this->destruct();
+                  }
+            }
+      }
+
+/**
+ * Provide a base class for models
+ */
+   class A_model extends A
+      {
+      /**
+       * Switch to toggle system::model()'s use of self::cleanup() each time the model is requested
+       * @access public
+       * @var boolean
+       */
+         public $cleanup=false;
+
+      /**
+       * Dummy method, extend and overload to make use of it (and read above comment)
+       * @access public
+       * @return void
+       */
+         public function cleanup() {}
+      }
+
+/**
+ * Provide a base class for controllers
+ */
+   class A_controller extends A {}
+
+/**
+ * Provide a base class for automators
+ */
+   class A_automator extends A {}
+
+/**
+ * Provide a base class for injectors
+ */
+   class A_injector extends A
+      {
+      /**
+       * Simplify the constructor from the parent since we don't want to run injectors inside themselves (infinite loop)
+       * @access public
+       * @return void
+       */
+         public function __construct(&$_)
+            {
+            // Set the reference to the universal array
+               $this->_=&$_;
+
+            // Load the system model
+               $this->system=&$this->_['objects']['models']['system'];
+
+            // Run our user defined constructor
+               $this->construct();
+            }
+
+      /**
+       * Runs before every object's construction
+       * @access public
+       * @return void
+       */
+         public function pre_construct(&$object) {}
+
+      /**
+       * Runs after every object's construction
+       * @access public
+       * @return void
+       */
+         public function post_construct(&$object) {}
+
+      /**
+       * Runs before every object's destruction
+       * @access public
+       * @return void
+       */
+         public function pre_destruct(&$object) {}
+
+      /**
+       * Runs after every object's destruction
+       * @access public
+       * @return void
+       */
+         public function post_destruct(&$object) {}
+
+      /**
+       * Simplify the destructor from the parent since we don't want to run injectors inside themselves (infinite loop)
+       * @access public
+       * @return void
+       */
+         public function __destruct()
+            {
+               $this->destruct();
             }
       }
 ?>
